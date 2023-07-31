86ee0ff131d6c5996de3b302417ba04f01ad1d2f
Geoff George <geoffgeorgethommana@gmail.com>
2023-07-31 15:52:32 +0530

    backend

--- api/index.js
+++ api/index.js
@@ -1,11 +1,23 @@
 
 const express=require("express");
+const cors=require("cors");
 
 const app=express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/',(req,res)=>{
-    res.json('test o2k')
+    res.json('hello world1')
 })
 
 
-app.listen(4000);
+app.post('/register',(req,res)=>{
+    const {username,password}=req.body;
+    res.json({reqData:{username,password}});
+})
+
+
+app.listen(4000,()=>{
+    console.log('listening on port 4000');
+});
