5f324ce8f46c6bf581371e11e90fce0345caaf4c
Geoff George <geoffgeorgethommana@gmail.com>
2023-07-31 16:14:32 +0530

    db

--- api/index.js
+++ api/index.js
@@ -1,23 +1,35 @@
 
 const express=require("express");
 const cors=require("cors");
+const { default: mongoose } = require("mongoose");
+const User=require('./models/User');
 
 const app=express();
 
+
+
+
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect('mongodb+srv://geoffgeorge107:Ag2M7UvZx9JdxY4L@cluster1.r6awora.mongodb.net/?retryWrites=true&w=majority')
+
 
 app.get('/',(req,res)=>{
     res.json('hello world1')
 })
 
 
-app.post('/register',(req,res)=>{
+app.post('/register',async(req,res)=>{
     const {username,password}=req.body;
-    res.json({reqData:{username,password}});
+    const UserDoc=await User.create({username,password});
+    res.json(UserDoc);
 })
 
 
 app.listen(4000,()=>{
     console.log('listening on port 4000');
 });
+// Ag2M7UvZx9JdxY4L
+
+// mongodb+srv://geoffgeorge107:Ag2M7UvZx9JdxY4L@cluster1.r6awora.mongodb.net/?retryWrites=true&w=majority
