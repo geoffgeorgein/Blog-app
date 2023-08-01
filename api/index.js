236aa76e38938ad1ababe549c526794b84bb292b
Geoff George <geoffgeorgethommana@gmail.com>
2023-08-01 16:30:02 +0530

    login

--- api/index.js
+++ api/index.js
@@ -1,16 +1,18 @@
 
 const express=require("express");
 const cors=require("cors");
-const { default: mongoose } = require("mongoose");
+const  mongoose = require("mongoose");
 const User=require('./models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const app=express();
 
 const salt=bcrypt.genSaltSync(10);
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://geoffgeorge107:Ag2M7UvZx9JdxY4L@cluster1.r6awora.mongodb.net/?retryWrites=true&w=majority')
@@ -30,6 +32,30 @@ app.post('/register',async(req,res)=>{
     });
     res.json(UserDoc);
 })
+
+app.post('/login',async(req, res)=>{
+    
+    const {username,password} = req.body;
+    const userDoc=await User.findOne({username});
+
+    const passOK=bcrypt.compareSync(password,userDoc.password);
+    // res.json(passOK);
+
+    if(passOK){
+        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+            if (err) throw err;
+            res.cookie('token', token).json({
+              id:userDoc._id,
+              username,
+            });
+          });
+    }
+    else {
+        res.status(400).json("wrong credentials");
+      }
+
+}
+);
 
 
 app.listen(4000,()=>{
