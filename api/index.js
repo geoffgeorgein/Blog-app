cacd15119cc27dfeeb96f07c123b076ab2cfa1ec
Geoff George <geoffgeorgethommana@gmail.com>
2023-08-05 15:54:17 +0530

    loggedIN

--- api/index.js
+++ api/index.js
@@ -75,6 +75,10 @@ app.get('/profile',(req, res) => {
 }
 )
 
+app.post('/logout',(req,res)=>{
+    res.cookie('token','').json('ok');
+})
+
 app.listen(4000,()=>{
     console.log('listening on port 4000');
 });
