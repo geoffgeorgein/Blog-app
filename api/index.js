01943a9fc6211d7113e057ee539bc0b23d790675
Geoff George <geoffgeorgethommana@gmail.com>
2023-08-15 15:08:19 +0530

    all

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ const salt=bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({credentials:true,origin:'https://blog-m8ji.onrender.com/'}));
 app.use(express.json());
 app.use('/uploads', express.static(__dirname + '/uploads'));    
 
